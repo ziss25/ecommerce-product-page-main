a0caf22a1e0edae86ddea4b1e268d0c040248b97
ziss aja <ziss2517@gmail.com>
2023-01-19 16:59:12 +0800

    header component cart days2

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+// menu action
 const menu = document.querySelector('.hamb-icon');
 const asside = document.querySelector('.nav-mobile');
 const close = document.querySelector('.close');
@@ -6,4 +7,12 @@ menu.addEventListener('click', () => {
 });
 close.addEventListener('click', () => {
   asside.classList.remove('end');
+});
+
+// cart action
+//dan urusan parent position nya udah di urus di css... karna css memiliki media query maka sangat gampang manipulasi lewat sana berdasarkan lebar layar tertentu
+const cart = document.querySelector('[cart]');
+const modal_cart = document.querySelector('[modal-cart]');
+cart.addEventListener('click', () => {
+  modal_cart.classList.toggle('none');
 });
