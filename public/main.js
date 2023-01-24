c0576960c49c0f1a677b910a761f6afaaa4c6196
ziss aja <ziss2517@gmail.com>
2023-01-24 18:19:06 +0800

    add future image product & beberapa des-text product

--- public/main.js
+++ public/main.js
@@ -16,3 +16,32 @@ const modal_cart = document.querySelector('[modal-cart]');
 cart.addEventListener('click', () => {
   modal_cart.classList.toggle('none');
 });
+
+// swipe js
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
+
+var swiper = new Swiper('.mySwiper-item', {
+  // loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+// ini tumb nya
+var swiper2 = new Swiper('.mySwiper-tumb', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+console.log(swiper2);
